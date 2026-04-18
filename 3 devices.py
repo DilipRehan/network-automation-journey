b42a795072e5a2df67ad5236f166b62b2f284f74
Dilip Rehan <dilip.rehan95@gmail.com>
2026-04-19 00:49:06 +0100

    a

--- 3 devices.py
+++ 3 devices.py
@@ -1,4 +1,9 @@
 from netmiko import ConnectHandler
+import subprocess
+
+file = open("show_version.txt", "w")
+
+
 
 routers = [
     {"device_type": "cisco_ios", "host": "192.168.138.130", "username": "admin", "password": "cisco123"}, 
@@ -10,6 +15,15 @@ for router in routers:
     try:
         connection = ConnectHandler(**router)
         print(f"Connected ! {router['host']}")
+              
+        output = connection.send_command("show version")
+        file.write("=" *40)
+        file.write(f"The output for :"+ router ['host']+"\n")
+        file.write(output)
+
+
+        file.write("=\n" *40)
+        file.close()
 
     except Exception as e:
         print (f"Error connecting to {router['host']}: and the error is {e}")
