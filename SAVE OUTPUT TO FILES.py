77e4ace59d7ddaf32f4bcc74f835eecf77632f9a
Dilip Rehan <dilip.rehan95@gmail.com>
2026-04-21 01:11:18 +0100

    1

--- SAVE OUTPUT TO FILES.py
+++ SAVE OUTPUT TO FILES.py
@@ -7,15 +7,23 @@ router = [
     {"device_type":"cisco_ios", "host":"192.168.138.136", "username":"admin", "password":"cisco123"}
 ]
 
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 for ip in router:
     try:
         connection = ConnectHandler(**ip)
         print(f"Connected to the {ip['host']} sucessfully")
-        line = connection.send_command("show version")
-        print(line)
+        output = connection.send_command("show ip interface brief")
 
-
-
+        file = f"Ouput{ip['host']}_{timestamp}.txt"
+        with open(file,"w") as f: 
+            f.write(f"Device {ip['host']}\n")
+            f.write(f"Timestamp: {timestamp}\n")
+            f.write("=" * 40 + "\n")
+            f.write(output)
+        
+        print(f"Saved to {file}")
+        connection.disconnect()
+        
     except Exception as e:
         print(f"\nThere is issue connecting to the {ip['host']} : {e}")
